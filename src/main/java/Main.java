54d540e97f092bd67dd7464b7c30a7da742b4f03
serendipity <1193131227@qq.com>
2021-04-30 19:19:49 +0800

    制定方向

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,10 +1,13 @@
 public class Main {
 
+    public static void main(String[] args) throws InterruptedException {
+        TankFrame tankFrame = new TankFrame();
 
-    public static void main(String[] args) {
 
-        new TankFrame();
-
+        while (true) {
+            Thread.sleep(100);
+            tankFrame.repaint();
+        }
 
     }
 }
--- src/main/java/TankFrame.java
+++ src/main/java/TankFrame.java
@@ -4,8 +4,7 @@ import java.awt.event.*;
 //集成frame 之后才能有构造里面的方法
 public class TankFrame extends Frame {
 
-    int x = 200;
-    int y = 200;
+    Tank tank = new Tank(800, 600, Dir.DOWN);
 
     public TankFrame() {
         setVisible(true);
@@ -13,20 +12,8 @@ public class TankFrame extends Frame {
         setTitle("Tank war");
         setSize(800, 600);
 
-
         //添加键盘监听器
-        this.addKeyListener(new KeyAdapter() {
-            @Override
-            public void keyPressed(KeyEvent e) {
-                System.out.println("key press");
-            }
-
-            @Override
-            public void keyReleased(KeyEvent e) {
-                System.out.println("key release");
-            }
-        });
-
+        addKeyListener(new MyKeyAdapter());
         //添加窗口监听器
         addWindowListener(new WindowAdapter() {
             @Override
@@ -38,11 +25,72 @@ public class TankFrame extends Frame {
 
     @Override
     public void paint(Graphics g) {
-        System.out.println("print");
-        g.fillRect(x, y, 50, 50);
-        x += 10;
-        y += 10;
+        //把画自己的逻辑放在tank里面，更加方便，提现了面向对象的封装性
+        tank.paint(g);
     }
 
 
+    class MyKeyAdapter extends KeyAdapter {
+
+        boolean bl = false;
+        boolean br = false;
+        boolean bu = false;
+        boolean bd = false;
+
+        @Override
+        public void keyPressed(KeyEvent e) {
+
+            int keyCode = e.getKeyCode();
+            switch (keyCode) {
+
+                case KeyEvent.VK_LEFT:
+                    bl = true;
+                    break;
+                case KeyEvent.VK_RIGHT:
+                    br = true;
+                    break;
+                case KeyEvent.VK_UP:
+                    bu = true;
+                    break;
+                case KeyEvent.VK_DOWN:
+                    bd = true;
+                    break;
+                default:
+                    break;
+            }
+            setMainTankDir();
+        }
+
+        @Override
+        public void keyReleased(KeyEvent e) {
+            int keyCode = e.getKeyCode();
+            switch (keyCode) {
+
+                case KeyEvent.VK_LEFT:
+                    bl = false;
+                    break;
+                case KeyEvent.VK_RIGHT:
+                    br = false;
+                    break;
+                case KeyEvent.VK_UP:
+                    bu = false;
+                    break;
+                case KeyEvent.VK_DOWN:
+                    bd = false;
+                    break;
+                default:
+                    break;
+            }
+            setMainTankDir();
+        }
+
+
+        private void setMainTankDir() {
+            if (bl) tank.setDir(Dir.LEFT);
+            if (br) tank.setDir(Dir.RIGHT);
+            if (bu) tank.setDir(Dir.UP);
+            if (bd) tank.setDir(Dir.DOWN);
+
+        }
+    }
 }
